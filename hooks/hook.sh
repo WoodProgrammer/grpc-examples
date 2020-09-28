e983124caf2cb893bf3b06cf82c64a35a98f0c36
WoodProgrammer <emirozbir@kloia.com>
2020-09-28 03:41:58 +0300

    updated hook

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -8,14 +8,17 @@ check_remote_master(){
   git log calculator --pretty=format:'%h' > /tmp/local_branch
   git fetch --all
   git log origin/master --pretty=format:'%h' > /tmp/remote_master
-  DIFF_REMOTE_MASTER=$(comm -23 /tmp/remote_master /tmp/local_branch)
 
-}
+  while IFS= read line; do
+    if grep -q $line "/tmp/local_branch"; then
+      echo "Exist"
+    else
+      echo "Hey One missing commit from remote master ${line}"
+    fi
 
-notify_developer(){
-  echo "Hello you are far away from the remote master ${DIFF_REMOTE_MASTER}"
+  done < /tmp/remote_master
 }
 
 #get_current_branch
 check_remote_master
-notify_developer
+
